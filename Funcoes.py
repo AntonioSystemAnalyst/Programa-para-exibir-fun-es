2c90a14e85d474d75d0f6be444dcaf1f6b744f67
Jose Antonio <j.thony.orpheu@gmail.com>
2022-01-25 18:26:26 -0300

    Linear funcionando

--- Funcoes.py
+++ Funcoes.py
@@ -1,5 +1,6 @@
-from random import random
+import random
 from re import X
+from tkinter import Y
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import math
@@ -28,14 +29,52 @@ def sequencia ():
     plt.show()
 
 
-def linear_Func():
-    print ('CHEGOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOUuuuuuuuuuuuuuuuuuuuuuuuuuuu')
-    x = random(1,10)
-    result = []
-    for i in range (4):
-        result[i] = x + 1
-    print (result)  
+def linear_Func():  # y = x + 1 rnd em x inicial 
+    result = [1, 2, 3, 4, 5, 6]
+    rnd = random.randrange (1,10)
+    ax = random.randrange(1,10)
+    i = 0
+    for i in range (6):
+        if ax % 2 == 0:
+            result[i] = rnd - i
+        else:
+            result[i] = rnd + i 
     return result
+
+def Parab_Func():  # y = ax^2 + bx + c 
+
+    a = random.randrange(1, 10)
+    b = random.randrange(1, 10)
+    c = random.randrange(1, 10)
+    x = -b/(2*a)
+    y = -1*(b*b-(4*a*c))/(4*a)
+    resultx = [1, 2, 3, 4, 5, 7, 8]
+    resulty = [1, 2, 3, 4, 5, 7, 8]
+    resultx [4] = x 
+    resulty [4] = y 
+    i = 5
+    for i in range (3):
+        resultx[i] = x + i
+        resulty[i] = y + i
+    return resultx, resulty
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
     
 
       
--- Tela.py
+++ Tela.py
@@ -1,17 +1,14 @@
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter import *
-from Funcoes import linear_Func, sequencia
+from Funcoes import *
 
 try:
     from matplotlib.backends.backend_tkagg import NavigationToolbar2TkAgg
 except ImportError:
     from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk as NavigationToolbar2TkAgg
 
-
 root = Tk()
-
-resultado = [1, 2, 3, 4, 5]
 
 Figura = plt.Figure(figsize=(8, 4), dpi= 60)
 ax = Figura.add_subplot (111)
@@ -20,16 +17,36 @@ canva.get_tk_widget().place(relx=0.25, rely= 0.2)
 toolbar = NavigationToolbar2TkAgg (canva, root)
 canva._tkcanvas.place(relx=0.25, rely= 0.2)
 
-def comunicacao ():
-    ax.clear()
-    resultado = linear_Func
-    Y = [1, 2, 7, 6, 1]
-    X = [1, 2, 3, 4, 5]
-    ax.plot(X, Y)
-    ax.legend()
-    canva.draw()
+
+resultado = [1, 2, 3, 4, 5]
+
 class Front():
 
+    def comunicacao1 (self):
+       
+        resultado = linear_Func()
+        Y = [1, 2, 7, 6, 8, 9]
+        X = [0, 1, 2, 3, 4, 5]
+        Y =  resultado
+        i = 0
+
+        ax.clear()
+        ax.plot(X, Y)
+        ax.legend()
+        canva.draw()
+    
+    def comunicacao2 (self):
+       
+        x, y = Parab_Func()
+        Y = [1, 2, 7, 6, 8, 9]
+        X = [0, 1, 2, 3, 4, 5]
+        Y =  resultado
+        i = 0
+
+        ax.clear()
+        ax.plot(x, y)
+        ax.legend()
+        canva.draw()
 
     def __init__(self):
         self.root = root
@@ -37,7 +54,6 @@ class Front():
         self.Labels()
         self.botoes()
         self.grafico()
-        
         root.mainloop()
 
     def tela(self):
@@ -52,16 +68,16 @@ class Front():
         self.Label1.place(relx=0.015, rely= 0.05, relwidth=0.30, relheight=0.1)
 
     def botoes(self):
-        self.btn1 = Button(self.root, text = "Linear", command=comunicacao)
+        self.btn1 = Button(self.root, text = "Linear", command=self.comunicacao1)
         self.btn1.place(relx=0.015, rely= 0.2, relwidth=0.15, relheight=0.1)
 
-        self.btn2 = Button(self.root, text = "Parabola", command=sequencia)
+        self.btn2 = Button(self.root, text = "Parabola", command=self.comunicacao2)
         self.btn2.place(relx=0.015, rely= 0.32, relwidth=0.15, relheight=0.1)
     
-        self.btn3 = Button(self.root, text = "Logaritma", command=sequencia)
+        self.btn3 = Button(self.root, text = "Logaritma")
         self.btn3.place(relx=0.015, rely= 0.435, relwidth=0.15, relheight=0.1)
         
-        self.btn4 = Button(self.root, text = "Exponenical", command=sequencia)
+        self.btn4 = Button(self.root, text = "Exponenical", command="")
         self.btn4.place(relx=0.015, rely= 0.559, relwidth=0.15, relheight=0.1)
     
     def grafico(self):
